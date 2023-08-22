c1c92fd01c4d4f9936d94dc9f89d07778fe2e29f
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-08-22 16:21:36 -0400

    finished the second excersize

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let loopedString = "";
+    let loopAmt = num;
 
+    if (loopAmt < 0) {
+        return "ERROR";
+    }
+    else {
+        for (let i = 0; i < loopAmt; i++) {
+            loopedString += string;
+        }
+        return loopedString;
+    }
 };
 
 // Do not edit below this line
