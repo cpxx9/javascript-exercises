572364c44b437e1e280d915ccc866fb8ad3710da
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-10-18 15:47:22 -0400

    Finished Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringArr = string.match(/[A-Za-z0-9]/g);
+    
+    let reverseString = stringArr.slice().reverse();
+    
+    return (stringArr.toString().toLowerCase() === reverseString.toString().toLowerCase());
 };
 
 // Do not edit below this line
