42518be3bea16577db7e1a3a00240b7d3771fea1
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-10-17 20:35:23 -0400

    finished the calculator excersize

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -13,12 +13,28 @@ const sum = function(arr) {
 const multiply = function(arr) {
   return arr.reduce((total, current) => total * current, 1);
 }
-const power = function() {
-	
+const power = function(a, b) {
+	let arr = [];
+  for (let i = 0; i < b; i++) {
+    arr.push(a);
+  }
+  return multiply(arr);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) { 
+    return 1;
+  } else {
+    let arr = [];
+    let arrCounter = num;
+
+    for (let i = 0; i < num; i++) {
+      arr.push(arrCounter);
+      arrCounter--;
+    }
+    
+    return arr.reduce((total, current) => total * current, 1);
+  }
 };
 
 // Do not edit below this line
