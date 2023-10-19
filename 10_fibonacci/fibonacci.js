85dfe394e96727c6697ad1e759413b987e6b0ac0
Cole <127376939+cpxx9@users.noreply.github.com>
2023-10-18 21:01:33 -0400

    started fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let sequence = [];
+    let counter = 0;
+    for(let i = 0; i < input; i++) {
+        if(i === 0) {
+            sequence[i] = 1;
+            counter++;
+        } else {
+            sequence[i] = counter + sequence[i - 1];
+            counter++;
+        }
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+/*
+function that takes an int as input 
+    create an empty array
+    loop adding elements to array for the length of input int
+        make the element equal to     
+*/
