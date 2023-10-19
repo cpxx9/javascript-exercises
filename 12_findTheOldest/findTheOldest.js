3a070d07a305eeacbab67c177f7aff1ea1885e7a
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-10-19 11:23:01 -0400

    finished find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let sortedAge = arr.sort(function(a, b) {
+        let last;
+        let next;
+        if ("yearOfDeath" in a) {
+            last = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            last = ((new Date()).getFullYear()) - a.yearOfBirth;
+        }
+        if ("yearOfDeath" in b) {
+            next = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            next = ((new Date()).getFullYear()) - b.yearOfBirth;
+        }
+        return last > next ? -1 : 1;
+    });
+    return sortedAge[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/*
+ function that takes an array as an input
+    each object in the array
+        subtract birth from death year to get age
+    return object with the highest age
+ */
